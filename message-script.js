9e3c34e8917603099c68a4660502c703ce3a621f
Igor Graziano <91213886+IgorRSGraziano@users.noreply.github.com>
2022-01-07 16:47:45 -0300

    fix informations

--- message-script.js
+++ message-script.js
@@ -4,13 +4,13 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       Igor
-// @match        https://valentecolchoes.mandeumzap.com.br/
+// @match        https://SeuDominio.mandeumzap.com.br/
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 function timeOfDay() {
-  var hour = (new Date()).getHours();
-  var time = "";
+  let hour = (new Date()).getHours();
+  let time = "";
   if (hour < 12) {
     return time = "Bom dia";
   } else if (hour >= 12 && hour < 18) {
@@ -23,4 +23,4 @@ function timeOfDay() {
 let textArea = document.querySelectorAll(".reply-text-area");
 let clientName = document.querySelector(".chat-header").querySelector("span[data-testid='message-person-name']").firstChild.nodeValue
 
-textArea[0].value = `${timeOfDay()} ${clientName}, tudo bem com você? Aqui é o Igor da Valente Outlet`;
+textArea[0].value = `${timeOfDay()} ${clientName}, tudo bem com você?`
